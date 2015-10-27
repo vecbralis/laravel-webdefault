354801a14775c4000c9890e997d324c60d968a07
Mkwebsites.lv <martins@mkwebsites.lv>
2015-10-27 12:19:25 +0200

    Changes in composer file

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,2 +1,19 @@
 <?php
 
+Route::group([
+	'namespace'  => '\MVsoft\Webdefault\Http\Controllers',
+	'middleware' => ['role:admin'],
+	], 
+function(){
+
+	//Load header
+	Route::get('webadmin/header', function(){
+		return view('mvsoft::partials.header');
+	});
+
+	//Load footer
+	Route::get('webadmin/footer', function(){
+		return view('mvsoft::partials.footer');
+	});
+
+});
