a79bf0c59bf64eb498334c2538b75904eb0c9a1d
Mkwebsites.lv <martins@mkwebsites.lv>
2015-10-29 21:51:28 +0200

    changes

--- src/Models/User.php
+++ src/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
